1fb2e92011aee20cbddad1c7cfdbf142c96c4233
alejanglez <64974849+alejanglez@users.noreply.github.com>
2020-10-30 19:00:59 +0100

    provider list

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -5,18 +5,18 @@ const User = require("../models/User.model");
 const Provider = require("../models/Provider.model");
 const Post = require("../models/Post.model");
 
-
 // ****************************************************************************************
 // POST route to submit the form to create a post
 // ****************************************************************************************
 
 // <form action="/post-create" method="POST">
-router.post('/create', (req, res) => {
+router.post("/create", (req, res) => {
   const { autor, reciver, comment, rating } = req.body;
   Post.create({ autor, reciver, comment, rating })
-    .then(dbPost => {
-
-      return Provider.findByIdAndUpdate(reciver, { $push: { posts: dbPost._id } });
+    .then((dbPost) => {
+      return Provider.findByIdAndUpdate(reciver, {
+        $push: { posts: dbPost._id },
+      });
     })
     .then((dbPost) => {
       res.status(201).json({ dbPost });
@@ -28,11 +28,10 @@ router.post('/create', (req, res) => {
     });
 });
 
-
-router.get('/list/:providerId', (req, res) => {
+router.get("/list/:providerId", (req, res) => {
   const { providerId } = req.params;
   Post.find({ providerId: providerId })
-    .populate('author') 
+    .populate("author")
     // .then(dbPosts => {
     //   console.log(dbPosts);
     //   res.render('posts/list', { posts: dbPosts });
@@ -42,7 +41,9 @@ router.get('/list/:providerId', (req, res) => {
       if (dbPostsList.length) {
         res.status(200).json({ dbPostsList });
       } else {
-        res.status(404).json({ errorMessage: "No resquested services were found" });
+        res
+          .status(404)
+          .json({ errorMessage: "No resquested services were found" });
       }
     })
     .catch((err) => {
@@ -58,19 +59,21 @@ router.get('/list/:providerId', (req, res) => {
 // shows how to deep populate (populate the populated field)
 // ****************************************************************************************
 
-router.get('/:postId', (req, res) => {
+router.get("/:postId", (req, res) => {
   const { postId } = req.params;
 
   Post.findById(postId)
-    .populate('author') 
-    .populate('reciver')
+    .populate("author")
+    .populate("reciver")
 
     .then((foundPost) => {
       res.status(201).json({ foundPost });
     })
 
     .catch((err) =>
-      console.log(`Err while getting a single requested service from the  DB: ${err}`)
+      console.log(
+        `Err while getting a single requested service from the  DB: ${err}`
+      )
     );
 });
 
