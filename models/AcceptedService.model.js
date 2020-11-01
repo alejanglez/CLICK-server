80102b2768f921947ad7351aa425db0193a9fe21
alejanglez <64974849+alejanglez@users.noreply.github.com>
2020-11-01 19:13:53 +0100

    accepted model

--- models/AcceptedService.model.js
+++ models/AcceptedService.model.js
@@ -4,8 +4,14 @@ const { Schema, model } = mongoose;
 
 const acceptedServiceSchema = new Schema(
   {
-    requestedserviceId: { type: mongoose.ObjectId, ref: "RequestedService", required: true },
-    serviceCat: { type: String, required: [true],
+    requestedserviceId: {
+      type: mongoose.ObjectId,
+      ref: "RequestedService",
+      required: true,
+    },
+    serviceCat: {
+      type: String,
+      required: [true],
       enum: [
         "Academic Support",
         "Informatics",
@@ -14,16 +20,20 @@ const acceptedServiceSchema = new Schema(
         "English Lessons",
         "Math Lessons",
         "Baby Sitting",
-        ],
+      ],
     },
-    lessonType:{ type: String, enum: [ "Online", "In-person"]},
+    lessonType: { type: String, enum: ["Online", "In-person"] },
     rate: { type: Number, default: 0 },
     totalPrice: { type: Number, default: 0 },
     quantity: { type: Number, default: 0 },
+    userFirstName: { type: String, required: true, maxlength: 20 },
+    userLastName: { type: String, required: true, maxlength: 20 },
+    providerFirstName: { type: String, required: true, maxlength: 20 },
+    providerLastName: { type: String, required: true, maxlength: 20 },
   },
   {
     timestamps: true,
-  },
-  );
+  }
+);
 
-module.exports = model('AcceptedService', acceptedServiceSchema);
+module.exports = model("AcceptedService", acceptedServiceSchema);
--- routes/acceptedService.route.js
+++ routes/acceptedService.route.js
@@ -1,4 +1,3 @@
-
 const { Router } = require("express");
 const router = new Router();
 const mongoose = require("mongoose");
@@ -7,92 +6,112 @@ const Provider = require("../models/Provider.model");
 const Session = require("../models/Session.model");
 const AcceptedService = require("../models/AcceptedService.model");
 
+router.get("/user/list/:userId", (req, res) => {
+  const { userId } = req.params;
 
-router.get("/list/:userId", (req, res) => {
-    const { userId } = req.params;
-
-    console.log(userId);
-    AcceptedService.find({ userId: userId })
-      .then((acceptedServiceList) => {
-        if (acceptedServiceList.length) {
-          res.status(200).json({ acceptedServiceList });
-        } else {
-          res.status(404).json({ errorMessage: "No resquested services were found" });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
+  console.log(userId);
+  AcceptedService.find({ userId: userId })
+    .then((acceptedServiceList) => {
+      if (acceptedServiceList.length) {
+        res.status(200).json({ acceptedServiceList });
+      } else {
         res
-          .status(500)
-          .json({ errorMessage: "Internal error", errorDetail: err });
-      });
-  });
+          .status(404)
+          .json({ errorMessage: "No resquested services were found" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ errorMessage: "Internal error", errorDetail: err });
+    });
+});
 
-  router.get("/list/:providerId", (req, res) => {
-    const { providerId } = req.params;
-    console.log(providerId);
-    AcceptedService.find({ providerId: providerId })
-      .then((acceptedServiceList) => {
-        if (acceptedServiceList.length) {
-          res.status(200).json({ acceptedServiceList });
-        } else {
-          res.status(404).json({ errorMessage: "No resquested services were found" });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
+router.get("/user/list/:providerId", (req, res) => {
+  const { providerId } = req.params;
+  console.log(providerId);
+  AcceptedService.find({ providerId: providerId })
+    .then((acceptedServiceList) => {
+      if (acceptedServiceList.length) {
+        res.status(200).json({ acceptedServiceList });
+      } else {
         res
-          .status(500)
-          .json({ errorMessage: "Internal error", errorDetail: err });
-      });
-  });
+          .status(404)
+          .json({ errorMessage: "No resquested services were found" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ errorMessage: "Internal error", errorDetail: err });
+    });
+});
 
 router.post("/", (req, res) => {
-    const { userId, providerId, serviceCat, lessonType, rate, totalPrice, quantity } = req.body;
+  const {
+    userId,
+    providerId,
+    serviceCat,
+    lessonType,
+    rate,
+    totalPrice,
+    quantity,
+    userFirstName,
+    userLasttName,
+    providerFirstName,
+    providerLastName,
+    userimageUrl,
+    providerimageUrl,
+  } = req.body;
 
-    //controlling request data
-    if (!quantity || !userId || !providerId ) {
-      res.status(500).json({ errorMessage: " quantity is empty" });
-      return;
-    }
-    AcceptedService.create({ 
-      
-        userId,
-        providerId,
-        quantity,
-        serviceCat,
-        lessonType,
-        rate,
-        totalPrice,
-        quantity
+  //controlling request data
+  if (!quantity || !userId || !providerId) {
+    res.status(500).json({ errorMessage: " quantity is empty" });
+    return;
+  }
+  AcceptedService.create({
+    userId,
+    providerId,
+    quantity,
+    serviceCat,
+    lessonType,
+    rate,
+    totalPrice,
+    userFirstName,
+    userLasttName,
+    providerFirstName,
+    providerLastName,
+    userimageUrl,
+    providerimageUrl,
+  })
+    .then((acceptedService) => {
+      res.status(201).json({ acceptedService });
     })
-      .then((acceptedService) => {
-        res.status(201).json({acceptedService });
-        
-      })
-      .catch((err) => {
-        res
-          .status(500)
-          .json({ errorMessage: "Internal error", errorDetail: err });
-      });
-  });
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ errorMessage: "Internal error", errorDetail: err });
+    });
+});
 
-  router.get("/:acceptedServiceId", (req, res) => {
-    const { acceptedServiceId } = req.params;
-    console.log(acceptedServiceId);
-  
-    AcceptedService.findById(acceptedServiceId)
+router.get("/:acceptedServiceId", (req, res) => {
+  const { acceptedServiceId } = req.params;
+  console.log(acceptedServiceId);
+
+  AcceptedService.findById(acceptedServiceId)
     .populate("requestedserviceId")
-      
-      
-      .then((foundacceptedService) => {
-        res.status(201).json({ foundacceptedService });
-      })
-  
-      .catch((err) =>
-        console.log(`Err while getting a single accepted service from the  DB: ${err}`)
-      );
-  });
 
+    .then((foundacceptedService) => {
+      res.status(201).json({ foundacceptedService });
+    })
+
+    .catch((err) =>
+      console.log(
+        `Err while getting a single accepted service from the  DB: ${err}`
+      )
+    );
+});
 
 module.exports = router;
